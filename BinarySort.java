8294a24b743468bdbea5b72182b59fcbb96f78db
U10416005 <r3r346@gmail.com>
2016-05-15 00:55:34 +0800

    Seperate the Node
    Seperate the Node to the individual class

--- BinarySort.java
+++ BinarySort.java
@@ -1,39 +1,29 @@
 //U10416005ªL«Ø¦t
 public class BinarySort{
-	public Object element;
-	public Node root;
-	public Node left;
-	public Node right;
-	public BinarySort(){
-		
+	//Data field
+	public Node init;
+	//Constructor
+	public BinarySort(Object e){
+		init = new Node(e);
 	}
-	public BinarySort(Object element){
-		this(element,null,null);
-	}
-	public BinarySort(Object element,Node left,Node right){
-		element = element;
-		this.left = left;
-		this.right = right;
-	}
-	public void nodeSort(Object e){
-		root = new Node(e);
-	}
+	//method to Add the number to node
 	public Node Add(Node node,int e){
 		if (node == null) {
 			return node = new Node(e);
 		}
 		if (e < (Integer) node.element) {
-			node.right = Add(node.left, e);
+			node.left = Add(node.left, e);
 		} else {
-			node.left = Add(node.right, e);
+			node.right = Add(node.right, e);
 		}
 		return node;
 	}
+	//method to sort the number
 	public void sort(Node node) {
 		if (node != null) {
-			sort(node.right);
-			System.out.print(((Integer) node.element).intValue() + " ");
 			sort(node.left);
+			System.out.print(((Integer) node.element).intValue() + " ");
+			sort(node.right);
   		}
 	}
 }
