a3336467090a494b0e49e3e8f2fe68990e70010f
U10416005 <r3r346@gmail.com>
2016-05-14 11:48:41 +0800

    Write the class create the node
    Create the node and write the add method

---
+++ BinarySort.java
@@ -0,0 +1,33 @@
+//U10416005ªL«Ø¦t
+public class BinarySort{
+	public Object element;
+	public Node root;
+	public Node left;
+	public Node right;
+	public BinarySort(){
+		
+	}
+	public BinarySort(Object element){
+		this(element,null,null);
+	}
+	public BinarySort(Object element,Node left,Node right){
+		element = element;
+		this.left = left;
+		this.right = right;
+	}
+	public void nodeSort(Object e){
+		root = new Node(e);
+	}
+	public Node Add(Node node,int e){
+		if (node == null) {
+			return node = new Node(e);
+		}
+		if (e < (Integer) node.element) {
+			node.right = Add(node.left, e);
+		} else {
+			node.left = Add(node.right, e);
+		}
+		return node;
+	}
+	
+}
