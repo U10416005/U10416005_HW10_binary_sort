d003f77adc598d4d071504ebbac1b9e374671cc0
U10416005 <r3r346@gmail.com>
2016-05-14 12:00:57 +0800

    The new patch respond BinarySort
    The new patch respond BinarySort for sort method

---
+++ TestBinarySort.java
@@ -0,0 +1,12 @@
+//U10416005ªL«Ø¦t
+public class TestBinarySort{
+	public static void main(String[] args){
+		int[] dig ={1,3,2,4,6,5,7,9,8,10};
+		BinarySort bs = new BinarySort(new Integer(dig[0]));
+		bs.nodeSort(new Integer(dig[0]));
+		for(int i = 1;i<dig.length;i++){
+			bs.Add(bs.root,new Integer(dig[i]));
+		}
+		bs.sort(bs.root);
+	}
+}
