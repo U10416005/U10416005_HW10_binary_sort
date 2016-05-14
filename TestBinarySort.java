2073105a3cba38a5a13256fa2401687ffad9bbe8
U10416005 <r3r346@gmail.com>
2016-05-14 12:03:03 +0800

    The test class for BinarySort
    The test class for BinarySort,give the nuber

--- TestBinarySort.java
+++ TestBinarySort.java
@@ -3,10 +3,8 @@ public class TestBinarySort{
 	public static void main(String[] args){
 		int[] dig ={1,3,2,4,6,5,7,9,8,10};
 		BinarySort bs = new BinarySort(new Integer(dig[0]));
-		bs.nodeSort(new Integer(dig[0]));
-		for(int i = 1;i<dig.length;i++){
+		for(int i = 0;i<dig.length;i++){
 			bs.Add(bs.root,new Integer(dig[i]));
 		}
-		bs.sort(bs.root);
 	}
 }
