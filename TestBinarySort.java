a9884ac1ba3905f16bdd3384fad230f95e34cc48
U10416005 <r3r346@gmail.com>
2016-05-16 13:54:41 +0800

    Respond the BinarySort patch
    Respond the BinarySort patch,call the nodeSort method

--- TestBinarySort.java
+++ TestBinarySort.java
@@ -6,6 +6,7 @@ public class TestBinarySort{
 		int[] dig ={1,3,2,4,6,5,7,9,8,10};
 		//invoke BinarySort
 		BinarySort bs = new BinarySort(new Integer(dig[0]));
+		bs.nodeSort(new Integer(dig[0]));
 		//add the number into Node
 		for(int i = 1;i<dig.length;i++){
 			bs.Add(bs.init,new Integer(dig[i]));
